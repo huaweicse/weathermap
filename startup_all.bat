6a083b6065aa2ec37c7aaf755fe3cb0353e819d0
l00247303 <83488245@qq.com>
2017-12-22 21:19:31 +0800

    support proxy, yellow zone need proxy setting

--- startup_all.bat
+++ startup_all.bat
@@ -3,7 +3,7 @@ REM
 REM Copyright@2017 Huawei Technologies Co., Ltd. 
 REM
 
-REM read system 
+REM read system
 if not "%ak%" == "" goto microservice
 if not "%sk%" == "" goto microservice
 
@@ -21,30 +21,60 @@ REM    accessKey: UZLI6CLENEO6D2ANV35U
 REM    secretKey: nAQ7jQyynhuDckGGtzNYMJGDZoFqaEfNwhadIXXP
 REM    akskCustomCipher: default
 :microservice
-echo cse: > microservice.yaml
-echo   credentials: >> microservice.yaml
-echo     accessKey: %ak% >> microservice.yaml
-echo     secretKey: %sk% >> microservice.yaml
-echo     akskCustomCipher: default >> microservice.yaml
+echo cse:> microservice.yaml
+echo   credentials:>> microservice.yaml
+echo     accessKey: %ak%>> microservice.yaml
+echo     secretKey: %sk%>> microservice.yaml
+echo     akskCustomCipher: default>> microservice.yaml
 
+REM read httpproxy properties
+if not exist "httpproxy.properties" goto nohttpproxy
+for /f "tokens=1,2 delims==" %%i in (httpproxy.properties) do (
+  set %%i=%%j
+)
+
+echo %proxy.enabled%
+
+if not "%proxy.enabled%" == "true" goto nohttpproxy
+if "%proxy.host%" == "" goto httpproxyerror
+if "%proxy.port%" == "" goto httpproxyerror
+
+if not exist ".\weather\conf\" md .\weather\conf\
+copy httpproxy.properties .\weather\conf\
+
+if not exist ".\weather-beta\conf\" md .\weather-beta\conf\
+copy httpproxy.properties .\weather-beta\conf\
+
+if not exist ".\forecast\conf\" md .\forecast\conf\
+copy httpproxy.properties .\forecast\conf\
+
+REM  proxy:
+REM    enable: true
+REM    host: proxy.huawei.com
+REM    port: 8080
+REM    username: l00247303
+REM    passwd: Huawei012.
+
+echo   proxy:>> microservice.yaml
+echo     enable: %proxy.enabled%>> microservice.yaml
+echo     host: %proxy.host%>> microservice.yaml
+echo     port: %proxy.port%>> microservice.yaml
+echo     username: %proxy.user%>> microservice.yaml
+echo     passwd: %proxy.password%>> microservice.yaml
+
+goto microservice_copy
+
+:nohttpproxy
+if exist ".\weather\conf\httpproxy.properties" del .\weather\conf\httpproxy.properties
+if exist ".\weather-beta\conf\httpproxy.properties" del .\weather-beta\conf\httpproxy.properties
+if exist ".\forecast\conf\httpproxy.properties" del .\forecast\conf\httpproxy.properties
+
+:microservice_copy
 copy microservice.yaml .\weather\
 copy microservice.yaml .\weather-beta\
 copy microservice.yaml .\forecast\
 copy microservice.yaml .\fusionweather\
 del microservice.yaml
-
-if not exist "httpproxy.properties" goto nohttpproxy
-
-REM read httpproxy settings
-for /f "tokens=1,2 delims==" %%i in (httpproxy.properties) do (
-  set %%i=%%j
-)
-
-
-
-:nohttpproxy
-if not "%ak%" == "" goto microservice
-
 
 REM mklink with all service
 if not exist "weather\lib" mklink /J weather\lib lib
@@ -70,8 +100,11 @@ start weathermapweb\startup.bat
 
 goto end
 
+:httpproxyerror
+echo "httpproxy config error."
+goto end
+
 :nocredentials
 echo "Cannot find ak/sk in credentials file or system environment variables."
-
 
 :end
