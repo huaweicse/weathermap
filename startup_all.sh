3f55d864d61ca19d9e369d434002a91a849b1115
l00247303 <83488245@qq.com>
2017-12-22 22:13:21 +0800

    http proxy settings

--- startup_all.sh
+++ startup_all.sh
@@ -8,7 +8,8 @@ if [ -z "$AK" ]; then
     if [ -e "credentials" ]; then 
         AK=`awk -F= '/ak/{print $2}' credentials`
     else
-	echo "Cannot find ak/sk in credentials file or system environment variables"
+	    echo "Cannot find ak/sk in credentials file or system environment variables"
+		exit 1
     fi
 fi
 
@@ -17,15 +18,34 @@ if [ -z "$SK" ]; then
         SK=`awk -F= '/sk/{print $2}' credentials`
     else
         echo "Cannot find ak/sk in credentials file or system environment variables"
+		exit 1
     fi
 fi
 
 # prepare microservice.yaml
 echo "cse:" > microservice.yaml
 echo "  credentials:" >> microservice.yaml
-echo "    accessKey: $AK">> microservice.yaml
-echo "    secretKey: $SK">> microservice.yaml
+echo "    accessKey: $AK" >> microservice.yaml
+echo "    secretKey: $SK" >> microservice.yaml
 echo "    akskCustomCipher: default" >> microservice.yaml
+
+if [ -e "httpproxy.properties" ]; then
+    PROXY_ENABLED=`awk -F= '/proxy.enabled/{print $2}' httpproxy.properties`
+	if [ "$PROXY_ENABLED" == "true" ]; then
+        PROXY_HOST=`awk -F= '/proxy.host/{print $2}' httpproxy.properties`
+        PROXY_PORT=`awk -F= '/proxy.port/{print $2}' httpproxy.properties`
+        PROXY_USER=`awk -F= '/proxy.user/{print $2}' httpproxy.properties`
+        PROXY_PASSWORD=`awk -F= '/proxy.password/{print $2}' httpproxy.properties`
+        echo "  proxy:" >> microservice.yaml
+        echo "    enable: $PROXY_ENABLED" >> microservice.yaml
+        echo "    host: $PROXY_HOST" >> microservice.yaml
+        echo "    port: $PROXY_PORT" >> microservice.yaml
+        echo "    username: $PROXY_USER" >> microservice.yaml
+        echo "    passwd: $PROXY_PASSWORD" >> m icroservice.yaml
+    else
+	    
+    fi
+fi
 
 # cp microservice
 cp microservice.yaml ./weather/
