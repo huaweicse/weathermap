64bd53582eb4e4718905d1e2f11b0d2ae6c93c73
兴 <jeho0815@163.com>
2018-01-13 19:01:34 +0800

    Update startup.sh

--- weathermapweb/startup.sh
+++ weathermapweb/startup.sh
@@ -20,10 +20,5 @@ fi
 #swith to work directory
 cd `dirname "$0"`
 
-#check node version
-if [[ $(echo "`node --version 2>&1` v4.8.4" | tr " " "\n" | sort -V | head -n 1) == "v4.8.4" ]]; then
-    npm install
-    npm start >../logs/weathermapweb.log 2>&1 &
-else
-    echo 'Node version must be 4.8.4+.'
-fi
+mkdir -p ../logs
+npm start >../logs/weathermapweb.log 2>&1 &
