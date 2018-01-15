9129e3f3ec42e123aeb374214d91cbbba0a01cc6
dalianmao2008 <lijie38@huawei.com>
2018-01-15 19:33:45 +0800

    Create start.sh

---
+++ weathermapweb/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##
+# Copyright@2017 Huawei Technologies Co., Ltd. 
+##
+
+#set NODE_HOME
+#NODE_HOME=/opt/node-v4.8.4
+
+#check NODE_HOME & npm
+NPM='npm'
+if [ -z "`which npm`" ]; then
+    echo "`which npm`"
+    if [ -z $NODE_HOME ]; then
+            NPM=$NODE_HOME/bin/npm
+        else
+            echo "Cannot find npm command and NODE_HOME."
+    fi
+fi
+
+#swith to work directory
+cd `dirname "$0"`
+mkdir ../logs
+npm start
