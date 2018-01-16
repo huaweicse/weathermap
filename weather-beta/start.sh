83e2c7a54e29559261df138647e5de87d934603d
dalianmao2008 <lijie38@huawei.com>
2018-01-16 16:00:49 +0800

    Update start.sh

--- weather-beta/start.sh
+++ weather-beta/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # CMDVAR="-Djava.security.egd=file:/dev/./urandom","java -agentlib:jdwp=transport=dt_socket,address=0:8000,server=y,suspend=n -jar"
-java $CMDVAR -jar ./currentweatherdata-2.0.0.jar
+java $CMDVAR -jar ./weather-beta-2.0.0.jar
