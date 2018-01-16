747d43ec89b567fc0f37dd8c3375b7f4597094c2
dalianmao2008 <lijie38@huawei.com>
2018-01-16 15:59:29 +0800

    Update start.sh

--- forecast/start.sh
+++ forecast/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # CMDVAR="-Djava.security.egd=file:/dev/./urandom","java -agentlib:jdwp=transport=dt_socket,address=0:8000,server=y,suspend=n -jar"
-java $CMDVAR -jar ./forecastweatherdata-1.0.0.jar
+java $CMDVAR -jar ./forecast-1.0.0.jar
