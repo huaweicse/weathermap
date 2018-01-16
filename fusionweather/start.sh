ce92277dc7325c556f33af9aa2cbce0c5ba5b549
dalianmao2008 <lijie38@huawei.com>
2018-01-16 16:00:04 +0800

    Update start.sh

--- fusionweather/start.sh
+++ fusionweather/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # CMDVAR="-Djava.security.egd=file:/dev/./urandom","java -agentlib:jdwp=transport=dt_socket,address=0:8000,server=y,suspend=n -jar"
-java $CMDVAR -jar ./fusionweatherdata-1.0.0.jar
+java $CMDVAR -jar ./fusionweather-1.0.0.jar
