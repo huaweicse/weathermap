e60e7de675153d26ba44d3b96aa8eb83461deeca
qinling <yi5637@sina.com>
2018-06-08 14:13:52 +0800

    add english language

--- weathermapweb/views/js/weather-2.0.js
+++ weathermapweb/views/js/weather-2.0.js
@@ -1,28 +1,137 @@
 (function () {
+    var myApp = angular.module("app", ['chart.js', 'ui.bootstrap', 'pascalprecht.translate']);
+    myApp.config(["$translateProvider", function($translateProvider) {
+        $translateProvider.translations('en_us', {
+            "weatherDemo": "Weather forecast CSE-Demo-2.0",
+            "cityWeather": "City Weather",
+            "search": "Search",
+            "weatherDetails": "Current Weather Details And Forecast For The Next 5 Days",
+            "yourCity": "Your city name (all fight)",
+            "currentUV": "Current city UV rays",
+            "releaseTime": "Release Time",
+            "index": "Index",
+            "highestTemp": "The highest temperature",
+            "lowestTemp": "The lowest temperature",
+            "baseOn": "Based on",
+            "cse": "CSE",
+            "dataSource": "development,the weather data comes from",
+            "currentCity": "Current City",
+            "weather36H":"36 hours forecast for the future ",
+            "weather3D":"The next 3 days forecast list",
+            "weather5D":"The next 5 days forecast list",
+            "map3D":"Forecast chart of the next 3 days",
+            "map5D":"Forecast chart of the next 5 days",
+            "UVIndexAndExposureLevel":"Correspondence between UV index and exposure level:",
+            "lowVolume":"0-2: Low volume (suitable)",
+            "amount":"3-5: In the amount (go out hat or sunglasses)",
+            "highVolume":"6-7: High volume (sunscreen to be rubbed)",
+            "excess":"8-10: Excess (Avoid activities under the hot sun)",
+            "excess11":"> 11: Excess (try not to go out)",
+            "tempPieChart":"Temperature pie chart",
+            "tempCurve":"Temperature curve",
+            "windPieChart":"Wind speed pie chart",
+            "windSpeedCurve":"Wind speed curve",
+            "pressurePieChart":"Pressure pie chart",
+            "pressureCurve":"Pressure curve",
+            "prePieChart":"Precipitation pie chart",
+            "preCurve":"Precipitation curve",
+            "noData":"Failed to achieve the weather data"
+        });
+        $translateProvider.translations('zh_cn', {
+            "weatherDemo": "天气预报 CSE-Demo-2.0",
+            "cityWeather": "城市天气预报",
+            "search": "搜索",
+            "weatherDetails": "当前天气详情及未来5天的天气预报",
+            "yourCity": "您的城市名称（全拼）",
+            "currentUV": "当前城市紫外线",
+            "releaseTime": "发布时间",
+            "index": "指数",
+            "highestTemp": "最高温度",
+            "lowestTemp": "最低温度",
+            "baseOn": "基于",
+            "cse": "微服务引擎（CSE）",
+            "dataSource": "开发，天气数据来源于",
+            "currentCity": "当前城市",
+            "weather36H":"未来36小时预报 ",
+            "weather3D":"未来3天预报列表",
+            "weather5D":"未来5天预报列表",
+            "map3D":"未来3天预报趋势图",
+            "map5D":"未来5天预报趋势图",
+            "UVIndexAndExposureLevel":"紫外线指数和暴晒级数的对应关系:",
+            "lowVolume":"0-2: 低量（适宜）",
+            "amount":"3-5: 中量（外出需遮阳帽或太阳镜）",
+            "highVolume":"6-7: 高量（需涂擦防晒霜）",
+            "excess":"8-10: 过量（避免在烈日下活动）",
+            "excess11":">11: 超量（尽量不要外出）",
+            "tempPieChart":"温度饼图",
+            "tempCurve":"温度曲线",
+            "windPieChart":"风速饼图",
+            "windSpeedCurve":"风速曲线",
+            "pressurePieChart":"气压饼图",
+            "pressureCurve":"气压曲线",
+            "prePieChart":"降水量饼图",
+            "preCurve":"降水量曲线",
+            "noData":"获取天气数据失败"
+        });
+        if (!window.localStorage.getItem("lang")) {
+            window.localStorage.setItem("lang", navigator.language.toLowerCase() == "en-us" ? "en_us" : "zh_cn");
+        }
+        var lang = window.localStorage.getItem("lang");
+        $translateProvider.preferredLanguage(lang);
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'i18n/',
+            suffix: '.json'
+        })
+    }]);
 
-    var myApp = angular.module("app", ["chart.js", 'ui.bootstrap']);
+    myApp.factory("T", ['$translate', function($translate){
+        var T = {
+            T: function(key){
+                if(key){
+                    return $translate.instant(key);
+                }
+                return key;
+            }
+        }
+        return T;
+    }]);
 
-    myApp.controller("LineCtrl", function ($scope, $http, $timeout, $filter, $window, $location) {
+    myApp.controller("LineCtrl", function ($scope, $http, $timeout, $filter, $window, $translate, T, $location) {
+
+        $scope.lang = window.localStorage.getItem("lang") == "zh_cn"?"English":"中文";
+        $scope.togglelang = function(){
+            if($translate.use()=="zh_cn"){
+                $translate.use("en_us");
+                window.localStorage.setItem("lang","en_us");
+                $scope.lang="中文";
+                window.location.reload();
+            }else{
+                $translate.use("zh_cn");
+                window.localStorage.setItem("lang","zh_cn");
+                $scope.lang="English";
+                window.location.reload();
+            }
+        }
 
         $scope.globalData = {
             city: "shenzhen",
             country: "CN",
             tempType: "C",
-            text1: "当前城市",
-            text2: "未来36小时预报",
-            text3: "未来5天预报列表",
-            text4: "未来5天预报趋势图",
-            text5: "紫外线指数和暴晒级数的对应关系:",
-            text6: "0-2: 低量（适宜）",
-            text7: "3-5: 中量（外出需遮阳帽或太阳镜）",
-            text8: "6-7: 高量（需涂擦防晒霜）",
-            text9: "8-10: 过量（避免在烈日下活动）",
-            text10: ">11: 超量（尽量不要外出）",
+            text1: T.T("currentCity"),
+            text2: T.T("weather36H"),
+            text3: T.T("weather5D"),
+            text4: T.T("map5D"),
+            text5: T.T("UVIndexAndExposureLevel"),
+            text6: T.T("lowVolume"),
+            text7: T.T("amount"),
+            text8: T.T("highVolume"),
+            text9: T.T("excess"),
+            text10: T.T("excess11"),
             refreshText: function () {
-                $scope.globalData.text1 = "当前城市 " + $scope.globalData.city + ", " + $scope.globalData.country;
-                $scope.globalData.text2 = "未来36小时预报 " + $scope.globalData.city + ", " + $scope.globalData.country;
-                $scope.globalData.text3 = "未来3天预报列表 " + $scope.globalData.city + ", " + $scope.globalData.country;
-                $scope.globalData.text4 = "未来3天预报趋势图 " + $scope.globalData.city + ", " + $scope.globalData.country;
+                $scope.globalData.text1 = T.T("currentCity") + $scope.globalData.city + ", " + $scope.globalData.country;
+                $scope.globalData.text2 = T.T("weather36")+ $scope.globalData.city + ", " + $scope.globalData.country;
+                $scope.globalData.text3 = T.T("weather3D") + $scope.globalData.city + ", " + $scope.globalData.country;
+                $scope.globalData.text4 = T.T("map5D") + $scope.globalData.city + ", " + $scope.globalData.country;
             }
         };
 
@@ -80,18 +189,18 @@
 
         $scope.forecastMainChartModel = {
             labels: [],
-            series: ['最高温度', '最低温度'],
+            series: [$scope.highestTemp, $scope.lowestTemp],
             data: [],
             onClick: function (points, evt) {
                 console.log(points, evt);
             },
             // colors: ['#45b7cd', '#ff6384', '#DCDCDC'],
             datasetOverride: [{
-                label: "最高温度",
+                label: $scope.highestTemp,
                 borderWidth: 2,
                 type: 'line'
             }, {
-                label: "最低温度",
+                label: $scope.lowestTemp,
                 borderWidth: 2,
                 type: 'line'
             }]
@@ -107,11 +216,11 @@
             data: [],
             // colors: ['#45b7cd', '#ff6384', '#DCDCDC'],
             datasetOverride: [{
-                label: "温度饼图",
+                label: T.T("tempPieChart"),
                 borderWidth: 1,
                 type: 'bar'
             }, {
-                label: "温度曲线",
+                label: T.T("tempCurve"),
                 borderWidth: 3,
                 hoverBackgroundColor: "rgba(255,99,132,0.4)",
                 hoverBorderColor: "rgba(255,99,132,1)",
@@ -121,23 +230,22 @@
             onChange: function (value) {
                 if (value == "Wind") {
                     $scope.forecastChartMultiModel.data = [g_pageData.v_barWindData, g_pageData.v_barWindData];
-                    $scope.forecastChartMultiModel.datasetOverride[0].label = "风速饼图";
-                    $scope.forecastChartMultiModel.datasetOverride[1].label = "风速曲线";
+                    $scope.forecastChartMultiModel.datasetOverride[0].label = T.T("windPieChart");
+                    $scope.forecastChartMultiModel.datasetOverride[1].label = T.T("windSpeedCurve");
                 }
                 else if (value == "Pressure") {
                     $scope.forecastChartMultiModel.data = [g_pageData.v_barPressureData, g_pageData.v_barPressureData];
-                    $scope.forecastChartMultiModel.datasetOverride[0].label = "气压饼图";
-                    $scope.forecastChartMultiModel.datasetOverride[1].label = "气压曲线";
+                    $scope.forecastChartMultiModel.datasetOverride[0].label = T.T("pressurePieChart");
+                    $scope.forecastChartMultiModel.datasetOverride[1].label = T.T("pressureCurve");
                 }
                 else if (value == "Precipitation") {
                     $scope.forecastChartMultiModel.data = [g_pageData.v_barPrecipitationData, g_pageData.v_barPrecipitationData];
-                    $scope.forecastChartMultiModel.datasetOverride[0].label = "降水量饼图";
-                    $scope.forecastChartMultiModel.datasetOverride[1].label = "降水量曲线";
-
+                    $scope.forecastChartMultiModel.datasetOverride[0].label = T.T("prePieChart");
+                    $scope.forecastChartMultiModel.datasetOverride[1].label = T.T("preCurve");
                 } else {
                     $scope.forecastChartMultiModel.data = [g_pageData.v_barTempData, g_pageData.v_barTempData];
-                    $scope.forecastChartMultiModel.datasetOverride[0].label = "温度饼图";
-                    $scope.forecastChartMultiModel.datasetOverride[1].label = "温度曲线";
+                    $scope.forecastChartMultiModel.datasetOverride[0].label = T.T("tempPieChart");
+                    $scope.forecastChartMultiModel.datasetOverride[1].label = T.T("tempCurve");
                 }
             }
         };
@@ -157,7 +265,8 @@
             }
             $http({
                 method: 'GET',
-                url: "/weathermapweb/ui/fusionweatherdata",
+                //url: "/weathermapweb/ui/fusionweatherdata",
+                url: "./mock/fusion_beta.json",
                 params: {"city": vCityName},
                 headers: {"demo": "2.0"},
                 timeout: 5000
@@ -304,7 +413,7 @@
 
                 }, function (err, stat) {
                     console.log(err);
-                    $scope.restAlertModel.list.push({msg: "Failed to achieve the weather data."});
+                    $scope.restAlertModel.list.push({msg:T.T("noData")});
                 }
             );
         }
